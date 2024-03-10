17a60d011a3f99e46fecde44fd13ec923c126982
Anet Taj <58222128+anettaj@users.noreply.github.com>
2024-03-10 11:55:45 +0530

    Changed the app name

--- flashlight/lib/main.dart
+++ flashlight/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlashLight',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -31,24 +31,70 @@ class FlashLight extends StatefulWidget {
 
 class _FlashLightState extends State<FlashLight> {
   bool isFlashlightOn = false;
-  final torchController = TorchController();
+  final TorchController torchController = TorchController();
+
+  @override
+  void initState() {
+    super.initState();
+    initFlashlight();
+  }
+
+  @override
+  void dispose() {
+    torchController.toggle(); // Turn off the flashlight
+    super.dispose();
+  }
+
+  Future<void> initFlashlight() async {
+    try {
+      bool? hasFlash = await torchController.hasTorch;
+
+      if (hasFlash != null && !hasFlash) {
+
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('No Flashlight Available'),
+            content: Text('This device does not have a camera flash.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    } catch (e) {
+      print('Failed to initialize flashlight: $e');
+    }
+  }
+
+  Future<void> toggleFlashlight() async {
+    try {
+      setState(() {
+        isFlashlightOn = !isFlashlightOn;
+      });
+
+      await torchController.toggle();
+    } catch (e) {
+      print('Failed to toggle flashlight: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: isFlashlightOn ? Colors.white :Colors.black ,
+      backgroundColor: isFlashlightOn ? Colors.white : Colors.black,
       body: Container(
         child: Center(
           child: IconButton(
-            onPressed: () {
-              setState(() {
-                isFlashlightOn = !isFlashlightOn;
-                torchController.toggle();
-              });
-            },
+            onPressed: toggleFlashlight,
             icon: isFlashlightOn
                 ? Icon(Icons.flashlight_on, size: 80,)
-                : Icon(Icons.flashlight_off_sharp, color: Colors.white, size: 80,)
+                : Icon(Icons.flashlight_off_sharp, color: Colors.white, size: 80,),
           ),
         ),
       ),
